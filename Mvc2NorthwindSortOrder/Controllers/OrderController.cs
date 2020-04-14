5238520188ec711c1153bf1747bf4b0f73ea7347
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-04-14 14:35:30 +0200

    Add project files.

---
+++ Mvc2NorthwindSortOrder/Controllers/OrderController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mvc2NorthwindSortOrder.Models;
+using Mvc2NorthwindSortOrder.ViewModels;
+
+namespace Mvc2NorthwindSortOrder.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public OrderController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        // GET
+        //
+        //Sortorder asc / desc
+        public IActionResult Index(string sortcolumn, string sortorder)
+        {
+            var orderListViewModel = new OrderListViewModel();
+
+            var items = _context.Orders.Include(order => order.Customer).Select(o=> new OrderListViewModel.OrderViewModel
+            {
+                CustomerName = o.Customer.CompanyName,
+                OrderDate = o.OrderDate,
+                OrderId = o.OrderId
+            });
+
+            if (string.IsNullOrEmpty(sortorder))
+                sortorder = "asc";
+
+
+            if (sortcolumn == "datum")
+            {
+                if(sortorder == "asc")
+                    items = items.OrderBy(p => p.OrderDate);
+                else
+                    items = items.OrderByDescending(p => p.OrderDate);
+            }
+            else if (sortcolumn == "id")
+            {
+                if (sortorder == "asc")
+                    items = items.OrderBy(p => p.OrderId);
+                else
+                    items = items.OrderByDescending(p => p.OrderId);
+
+            }
+            else 
+            {
+                if (sortorder == "asc")
+                    items = items.OrderBy(p => p.CustomerName);
+                else
+                    items = items.OrderByDescending(p => p.CustomerName);
+
+                sortcolumn = "namn";
+            }
+
+
+            orderListViewModel.Items = items.ToList();
+            orderListViewModel.SortColumn = sortcolumn;
+            orderListViewModel.SortOrder = sortorder;
+
+            return View(orderListViewModel);
+        }
+    }
+}
---
+++ Mvc2NorthwindSortOrder/ViewModels/OrderListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mvc2NorthwindSortOrder.ViewModels
+{
+    public class OrderListViewModel
+    {
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+        public List<OrderViewModel> Items { get; set; } = new List<OrderViewModel>();
+        public class OrderViewModel
+        {
+            public int OrderId { get; set; }
+            public string CustomerName { get; set; }
+            public DateTime? OrderDate { get; set; }
+
+        }
+    }
+}
