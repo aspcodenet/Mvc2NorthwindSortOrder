0365740305478229afb3f12763a7c57df54de8fb
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-04-24 10:29:24 +0200

    sökning

---
+++ Mvc2NorthwindSortOrder/Controllers/SupplierController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mvc2NorthwindSortOrder.Models;
+using Mvc2NorthwindSortOrder.ViewModels;
+
+namespace Mvc2NorthwindSortOrder.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public SupplierController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        // GET
+        public IActionResult Index(string searchWord)
+        {
+            var model = new SupplierListViewModel();
+            var query = _context.Suppliers.Select(r => new SupplierListViewModel.SupplierViewModel
+            {
+                Id = r.SupplierId,
+                City = r.City,
+                Company = r.CompanyName,
+                ContactName = r.ContactName,
+                Country = r.Country
+            });
+            if (!string.IsNullOrEmpty(searchWord))
+            {
+                query = query.Where(s => s.Country.Contains(searchWord) || s.Company.Contains(searchWord));
+            }
+            model.Items = query.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ Mvc2NorthwindSortOrder/ViewModels/SupplierListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Mvc2NorthwindSortOrder.ViewModels
+{
+    public class SupplierListViewModel
+    {
+        public string SearchWord { get; set; }
+        public List<SupplierViewModel> Items { get; set; } = new List<SupplierViewModel>();
+        public class SupplierViewModel
+        {
+            public int Id { get; set; }
+            public string Company { get; set; }
+            public string ContactName { get; set; }
+            public string City { get; set; }
+            public string Country { get; set; }
+        }
+    }
+}
